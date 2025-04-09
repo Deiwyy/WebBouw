6d6637128830901ec6606cb3921fbbd07b719778
Deiwyy <kaprolekdawidek@gmail.com>
2025-04-09 20:23:37 +0200

    updated to fit pythonanywhere

--- run.py
+++ run.py
@@ -1,6 +1,21 @@
-from app import create_app
+# This file contains the WSGI configuration required to serve up your
+# web application at http://<your-username>.pythonanywhere.com/
+# It works by setting the variable 'application' to a WSGI handler of some
+# description.
+#
+# The below has been auto-generated for your Flask project
 
-app = create_app()
+import sys
+
+# add your project directory to the sys.path
+project_home = '/home/dawidkaprol/mysite'
+if project_home not in sys.path:
+    sys.path = [project_home] + sys.path
+
+# import flask app but need to call it "application" for WSGI to work
+from app import create_app # noqa
+application = create_app()
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    application.run()
